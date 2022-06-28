297692ffe5cdfab52afe2256fa21c169f54d9b32
Manuel Hernandez <cmanuelhm87@gmail.com>
2022-06-28 15:32:18 +0200

    Hook personalizado usando estado

--- src/components/PruebasCustom.js
+++ src/components/PruebasCustom.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { useMayus } from "../hooks/useMayus";
 
 export const PruebasCustom = () => {
-  const { mayusculas, minusculas, concatenar } = useMayus("mAnUelHerNadez");
-
-  console.log(mayusculas());
-  console.log(minusculas());
-  console.log(concatenar(" holi"));
+  const { estado, mayusculas, minusculas, concatenar } =
+    useMayus("Manuel Her Mon");
 
   return (
     <div>
       <h1>Probando componentes personalizados</h1>
+      <h2>{estado}</h2>
+
+      <button onClick={mayusculas}>Poner en mayusuculas</button>
+      <button onClick={minusculas}>Poner en mayusuculas</button>
+      <button onClick={(e) => concatenar("- Probando Hooks Personalizados")}>
+        Poner en mayusuculas
+      </button>
     </div>
   );
 };
