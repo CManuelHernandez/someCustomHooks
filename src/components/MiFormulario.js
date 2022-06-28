9c36f8882d5f69b1cc679bbbd4fd5ac20a0abcc1
Manuel Hernandez <cmanuelhm87@gmail.com>
2022-06-28 16:42:40 +0200

    onChange en el formulario

--- src/components/MiFormulario.js
+++ src/components/MiFormulario.js
@@ -30,17 +30,52 @@ export const MiFormulario = () => {
     setFormulario(curso);
   };
 
+  const cambiado = ({ target }) => {
+    const { name, value } = target;
+
+    setFormulario({
+      ...formulario,
+      [name]: value,
+    });
+  };
+
   return (
     <div>
       <h1>Formulario</h1>
       <p>Formulario para guardar un curso</p>
-      <pre>{JSON.stringify(formulario)}</pre>
+      <p>Curso guardado: {formulario.titulo}</p>
+      <pre className="codigo">{JSON.stringify(formulario)}</pre>
       <form onSubmit={enviado} className="miFormulario">
-        <input type="texto" name="titulo" placeholder="Titulo" />
-        <input type="number" name="anio" placeholder="Año publicacion" />
-        <textarea name="descripcion" placeholder="Descripcion" />
-        <input type="texto" name="autor" placeholder="Autor" />
-        <input type="email" name="email" placeholder="Correo de contacto" />
+        <input
+          type="texto"
+          name="titulo"
+          onChange={cambiado}
+          placeholder="Titulo"
+        />
+        <input
+          type="number"
+          name="anio"
+          onChange={cambiado}
+          placeholder="Año publicacion"
+        />
+        <textarea
+          name="descripcion"
+          onChange={cambiado}
+          placeholder="Descripcion"
+        />
+        <input
+          type="texto"
+          name="autor"
+          onChange={cambiado}
+          placeholder="Autor"
+        />
+        <input
+          type="email"
+          name="email"
+          onChange={cambiado}
+          placeholder="Correo de contacto"
+        />
+
         <input type="submit" value="Enviar" />
       </form>
     </div>
