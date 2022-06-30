73d5a358a6b21b1638d5970abcfd69e754169276
Manuel Hernandez <cmanuelhm87@gmail.com>
2022-06-30 11:14:25 +0200

    Hook personalizado peticiones asincronas

--- src/components/MiUsuario.js
+++ src/components/MiUsuario.js
@@ -1,46 +1,22 @@
 import React, { useEffect, useState } from "react";
+import { useAjax } from "../hooks/useAjax";
 
 export const MiUsuario = () => {
-  const [usuario, setUsuario] = useState({
-    datos: null,
-    cargando: true,
-  });
-
-  const getUsuario = async (url) => {
-    setUsuario({
-      ...usuario,
-      cargando: true,
-    });
-
-    setTimeout(async () => {
-      const peticion = await fetch(url);
-      const { data } = await peticion.json();
-
-      setUsuario({
-        datos: data,
-        cargando: false,
-      });
-    }, 1500);
-  };
+  const [url, setUrl] = useState("https://reqres.in/api/users/1");
+  const { datos, cargando } = useAjax(url);
 
   const getId = (e) => {
     let id = parseInt(e.target.value);
-    let url = `https://reqres.in/api/users/${id}`;
-
-    getUsuario(url);
+    setUrl(`https://reqres.in/api/users/${id}`);
   };
-
-  useEffect(() => {
-    getUsuario("https://reqres.in/api/users/1");
-  }, []);
 
   return (
     <div>
       <h1>Mi usuario: </h1>
       <p>Datos del usuario:</p>
-      <p>{usuario.cargando ? "Cargando..." : ""}</p>
+      <p>{cargando ? "Cargando..." : ""}</p>
       <p>
-        {usuario?.datos?.first_name} {usuario?.datos?.last_name}
+        {datos?.first_name} {datos?.last_name}
       </p>
       <input type="number" name="id" onChange={getId} />
     </div>
